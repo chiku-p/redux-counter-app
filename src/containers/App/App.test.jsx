0686ba3f46032711d435a0165055e45ffad7148b
Chinmaya Pati <chinmaya.pati@loginradius.com>
2019-04-08 11:41:43 +0530

    Added test cases for saga

--- src/containers/App/App.test.jsx
+++ src/containers/App/App.test.jsx
@@ -9,24 +9,57 @@ import {
 
 // Store
 import { Provider } from "react-redux";
-import createStore from "../../store/createStore";
+import configureStore from "../../store/createStore";
 import App from ".";
 
 afterEach(cleanup);
 
-it("should create and update counter values", async () => {
-  // Initial state
-  const initialState = {
-    counters: [
-      { id: "counter1", value: 0 },
-      { id: "counter2", value: 10 },
-      { id: "counter3", value: 11 },
-      { id: "counter4", value: 12 }
-    ]
-  };
+/** Constants */
+const delay = ms => new Promise(rs => setTimeout(rs, ms));
+const initialState = {
+  counters: [
+    { id: "counter1", value: 0 },
+    { id: "counter2", value: 10 },
+    { id: "counter3", value: 11 },
+    { id: "counter4", value: 12 }
+  ]
+};
+const initialValues = initialState.counters.map(i => i.value);
 
+/** Test Cases */
+it("should increment counter after 1000ms delay", async () => {
   // Create a store
-  const store = createStore(initialState);
+  const store = configureStore(initialState);
+
+  store.dispatch({ type: "INCREMENT_COUNTER", payload: { id: "counter1" } });
+
+  const firstState = store.getState().counters.map(c => c.value);
+  expect(firstState).toEqual(initialValues);
+
+  await delay(1000);
+
+  const secondState = store.getState().counters.map(c => c.value);
+  expect(secondState).toEqual([1, 10, 11, 12]);
+});
+
+it("should decrement counter after 1000ms delay", async () => {
+  // Create a store
+  const store = configureStore(initialState);
+
+  store.dispatch({ type: "DECREMENT_COUNTER", payload: { id: "counter1" } });
+
+  const firstState = store.getState().counters.map(c => c.value);
+  expect(firstState).toEqual(initialValues);
+
+  await delay(1000);
+
+  const secondState = store.getState().counters.map(c => c.value);
+  expect(secondState).toEqual([-1, 10, 11, 12]);
+});
+
+it("should update counter values in the ui", async () => {
+  // Create a store
+  const store = configureStore(initialState);
 
   // Create the App component
   const { getByTestId } = render(
@@ -37,24 +70,29 @@ it("should create and update counter values", async () => {
 
   // Wait till component is mounted to DOM
   await Promise.all(
-    initialState.counters.map(c => {
-      return waitForElement(() => getByTestId("counter-container" + c.id));
-    })
+    initialState.counters.map(c =>
+      waitForElement(() => getByTestId("counter-container" + c.id))
+    )
   );
 
   // Test initial counter values
   const raw = initialState.counters.map(c =>
     Number(getByTestId("counter-value" + c.id).innerHTML)
   );
-  const expectedRawValues = [0, 10, 11, 12];
+  const expectedRawValues = [...initialValues];
   expect(raw).toEqual(expectedRawValues);
-
 
   // Fire click events
   fireEvent.click(getByTestId("incrementcounter1"));
+
   fireEvent.click(getByTestId("decrementcounter2"));
+  await delay(1000); // Since the INCREMENT action can not be triggered twice within 1000ms
+
   fireEvent.click(getByTestId("decrementcounter3"));
+  await delay(1000); // Same goes for DECREMENT action
+
   fireEvent.click(getByTestId("incrementcounter4"));
+  await delay(1000); // The final update action is triggered after 1000ms interval
 
   // Check the new values
   const newValues = initialState.counters.map(c =>
--- src/store/createStore.jsx
+++ src/store/createStore.jsx
@@ -6,25 +6,19 @@ import createSagaMiddleware from "redux-saga";
 import rootReducer from "../reducers";
 import rootSaga from "../sagas";
 
-function configureStore() {
-  let middlewares;
+function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
 
+  let middlewares = applyMiddleware(sagaMiddleware);
   if (process.env.NODE_ENV === "development") {
     middlewares = compose(
       applyMiddleware(sagaMiddleware, logger),
       window.__REDUX_DEVTOOLS_EXTENSION__ &&
         window.__REDUX_DEVTOOLS_EXTENSION__()
     );
-  } else {
-    middlewares = compose(
-      applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
   }
 
-  const store = createStore(rootReducer, middlewares);
+  const store = createStore(rootReducer, initialState || {}, middlewares);
 
   sagaMiddleware.run(rootSaga);
 
